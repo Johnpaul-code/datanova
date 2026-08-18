1483bea5255c1430746c0af3e71e0aa03c703dca
Johnpaul <johnpaulodoh36@gmail.com>
2026-08-17 18:20:25 -0700

    Initial commit

---
+++ JS/script.js
@@ -0,0 +1,45 @@
+const navigationLinks = document.querySelectorAll('.link')
+
+navigationLinks.forEach((link)=>{
+    const dropdown = link.querySelector('.dropdown');
+    
+    if(!dropdown) return
+
+    
+    let timeout = null
+
+    const show = () =>{
+        clearTimeout(timeout)
+        dropdown.classList.add('open')
+    }
+
+    const hide = () => {
+        dropdown.classList.remove('open')
+    }
+
+    const scheduleHide = () => {
+        clearTimeout()
+        timeout = setTimeout(hide, 200) //wait for 200ms
+    }
+
+    // 1. Show when entering link or dropdown
+    link.addEventListener('mouseenter', show)
+    dropdown.addEventListener('mouseenter', show)
+
+    // 2. Start timer when leaving link or dropdown
+    link.addEventListener('mouseleave', scheduleHide)
+    dropdown.addEventListener('mouseleave', scheduleHide)
+
+    // 3. Click anywhere outside to cole immediately
+    document.addEventListener('click', (e) => {
+        navigationLinks.forEach((link) => {
+            const dropdown = link.querySelector('.dropdown')
+            if(!dropdown) return
+
+            // if click is not inside this link or its dropdown
+            if(!link.contains(e.target)){
+                dropdown.style.display = 'none'
+            }
+        })
+    })
+})
